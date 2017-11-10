f75dde736524da6fd67ff21ff3ce22082d7a1f25
Shaswat Pandey <shaswat.pandey@drivezy.com>
2017-11-10 18:55:35 +0530

    Made changes to css added Image and changed some paths

--- shum/src/App.js
+++ shum/src/App.js
@@ -3,13 +3,15 @@ import React, { Component } from 'react';
 import './Assets/css/default.min.css';
 
 import Header from './Components/headerComponents/header.js'
-import Homepage from './Components/pages/homepage.js'
+import Home from './Components/pages/Homepage/home.js'
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header />
-        <Homepage />
+        <div className="top-container">
+          <Header />
+          <Home />
+        </div>
       </div>
     );
   }
---
+++ shum/src/Components/pages/Homepage/home.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+class Home extends Component {
+  render() {
+    return (
+      <div className="tag-line">
+        Be <span className="yellow">Secure</span>, Be <span className="red">Smart!</span>
+      </div>
+    );
+  }
+}
+
+
+export default Home;
